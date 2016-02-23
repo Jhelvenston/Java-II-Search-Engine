dbc9d6bf5a911a3a9e5b94623acc793b6a119b6c
James <James@Sphinx>
2016-02-22 21:18:31 -0500

    James Helvenston - more component declarations

--- src/searchengineproject/SearchEngineProject.java
+++ src/searchengineproject/SearchEngineProject.java
@@ -1,17 +1,18 @@
 package searchengineproject;
 
 import javax.swing.*;
+import java.awt.*;
 /*
  * Search engine project
  */
 public class SearchEngineProject
 {
-    public void init ()
-    {
-        //GUI components initialized here
-        JLabel searchEng = new JLabel ("Search Engine");
-        JLabel searchInd = new JLabel ("Search Engine Index");
-    }
+        //private JButton searchButton, maintenanceButton, aboutButton;
+        private JLabel titleLabel, termsLabel, indexedLabel;
+        private JRadioButton allRadio, anyRadio, exactRadio;
+        private JTextArea resultsArea;
+        private JTextField searchField;
+        private JPanel top;
     
     public static void main( String[] args )
     {
@@ -22,4 +23,32 @@ public class SearchEngineProject
         searchFrame.setVisible( true );
     }
     
+        public void search()
+    {
+        //GUI components initialized here
+        titleLabel = new JLabel( "Search Engine", JLabel.TOP );
+        searchField = new JTextField();
+        termsLabel = new JLabel();
+        indexedLabel = new JLabel();
+        allRadio = new JRadioButton();
+        anyRadio = new JRadioButton();
+        exactRadio = new JRadioButton();
+        resultsArea = new JTextArea();
+        JButton searchButton = new JButton();
+        JButton maintenanceButton = new JButton();
+        JButton aboutButton = new JButton();
+
+        titleLabel.setFont(new java.awt.Font("Tahoma", 0, 36));
+        termsLabel.setText("File Terms");
+        allRadio.setText("Search All File Terms");
+        anyRadio.setText("Search Any File Terms");
+        exactRadio.setText("Search Exact Phrase");
+
+        resultsArea.setColumns(20);
+        resultsArea.setRows(5);
+
+        searchButton.setText("Search");
+        maintenanceButton.setText("Maintenance");
+        aboutButton.setText("About");
+    }
 }
